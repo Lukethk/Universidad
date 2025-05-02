3aa473d7f0e912fde8e1c28e572655a0edce3eb3
Nogbre <hedernobre6@gmail.com>
2025-05-02 11:50:12 -0400

    Usando variables de entorno para conexión

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,29 +1,23 @@
 import sql from 'mssql';
-import sqlv8 from 'msnodesqlv8';
 
-// Definimos los parámetros de la base de datos
 const dbSettings = {
-    server: '127.0.0.1',  // Nombre del servidor
-    database: 'Inventario',  // Nombre de la base de datos
-    user: 'sa',  // El nombre de usuario que creaste
-    password: 'nuevaContraseñaFuerte',  // La contraseña que asignaste
-    driver: 'msnodesqlv8',  // El driver de conexión
-    options: {
-        trustServerCertificate: true  // Si es necesario
-    }
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  options: {
+    encrypt: true,
+    trustServerCertificate: true
+  }
 };
 
-
-// Función para obtener la conexión a la base de datos
 export const getConnection = async () => {
-    try {
-        // Intentamos conectarnos a la base de datos
-        const pool = await sql.connect(dbSettings);
-        console.log("✅ Conectado a SQL Server");
-        return pool;
-    } catch (error) {
-        // Si ocurre un error de conexión, lo mostramos
-        console.error("❌ Error connecting to SQL Server:", error);
-        return null;
-    }
+  try {
+    const pool = await sql.connect(dbSettings);
+    console.log("✅ Conectado a SQL Server");
+    return pool;
+  } catch (error) {
+    console.error("❌ Error connecting to SQL Server:", error);
+    return null;
+  }
 };
